ac57c13f280b9018435b6bb522a6463793451436
Collin McBroom <52358711+fishe-tm@users.noreply.github.com>
2023-04-23 13:35:54 -0600

    Update seafile-rhel9.sh

--- wip/seafile-rhel9.sh
+++ wip/seafile-rhel9.sh
@@ -10,7 +10,7 @@ run_check() {
 
 first_run() {
     printf "\n DISCLAIMER: This script is meant to be ran on a fresh install, and may not work properly if it is not ran on such. I am not responsible for anything that happens. \n"
-    echo "You should also have a partition pre-made to use for Seafile data" && sleep 10
+    printf "You should also have a partition pre-made to use for Seafile data" && sleep 10
 
     echo "Installing and configuring Docker..." && sleep 1
     sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -18,7 +18,7 @@ first_run() {
     sudo systemctl enable --now docker 
     sudo usermod -aG docker $(whoami)
     touch ~/sfscript_check_run
-    echo "\n\n System will reboot in 10 seconds for group changes to take effect. Terminate (Ctrl + C) to stop this.\n Run the script again to continue with configuration"
+    printf "\n\n System will reboot in 10 seconds for group changes to take effect. Terminate (Ctrl + C) to stop this.\n Run the script again to continue with configuration"
     sleep 10 && sudo reboot
 }
 
@@ -28,9 +28,9 @@ second_run() {
     docker compose up -d
     docker compose down
     clear; sudo blkid
-    echo "\n\nPlease copy the UUID of the drive partition you'd like to use for Seafile data and paste it here, without quotes"
+    printf "\n\nPlease copy the UUID of the drive partition you'd like to use for Seafile data and paste it here, without quotes"
     read -p "Enter here: " uuid
-    echo "\nNice! Now pick a place for the drive to be mounted (something like /mnt/seafile, DO NOT ADD trailing /)"
+    printf "\nNice! Now pick a place for the drive to be mounted (something like /mnt/seafile, DO NOT ADD trailing /)"
     read -p "Enter here: " drive_loc
 
     filesystem=$(lsblk -f /dev/disk/by-uuid/$uuid)
@@ -40,20 +40,20 @@ second_run() {
     echo "UUID=$uuid $drive_loc $filesystem  defaults  0 0" | sudo tee -a /etc/fstab
     sudo systemctl daemon-reload
     sudo mount -a
-
-    sudo chown -R $(whoami) $drive_loc
+ 
     sudo cp -r /opt/seafile-data /opt/seafile-data_bak
     sudo cp -r /opt/seafile-data $drive_loc/
     sudo rm -rf /opt/seafile-data
+    sudo chown -R $(whoami) $drive_loc/seafile-data/
     sed -i "s%- /opt/seafile-data:/shared%- $drive_loc/seafile-data:/shared%g" /srv/www/seafile/docker-compose.yml
     docker compose up -d
     
-    echo "\nYou will now create an admin user. Delete the default one after logging in at System Admin -> Users."
+    printf "\nYou will now create an admin user. Delete the default one after logging in at System Admin -> Users."
     docker exec -it seafile /opt/seafile/seafile-server-latest/reset-admin.sh
 }
 
 tailscale() {
-    echo "Installing tailscale, be ready to connect it to your tailnet" && sleep 2
+    printf "\nInstalling tailscale, be ready to connect it to your tailnet" && sleep 2
     sudo dnf config-manager --add-repo https://pkgs.tailscale.com/stable/rhel/9/tailscale.repo
     sudo dnf install -y tailscale
     sudo systemctl enable --now tailscaled
